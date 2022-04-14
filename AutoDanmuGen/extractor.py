c442a41c373e268902f4c85aa5b67480125295a9
flyotlin <flyotlin@gmail.com>
2022-04-14 11:27:31 +0800

    Rewrite the code to make it more understandable

--- AutoDanmuGen/extractor.py
+++ AutoDanmuGen/extractor.py
@@ -3,6 +3,7 @@ from AutoDanmuGen.common_vars import CommonVars
 
 class Extractor(object):
     """Extract frames and comments from raw `.mp4` and `.ass` files."""
+
     frames_outdir = os.path.join(CommonVars.tmp_dir, 'frames')
     comments_outfile = os.path.join(CommonVars.tmp_dir, 'comment.txt')
 
@@ -13,24 +14,29 @@ class Extractor(object):
             filepath (str): filepath to `.mp4` and `.ass` files, *but without extension*.
             video_id (int, optional): _description_. Defaults to 0.
         """
+
         self.filepath = filepath
         self.video_id = video_id
 
     def frames(self):
         """Extract frames from `.mp4` file and save them inside `tmp/frames`"""
-        if not os.path.exists(self.frames_outdir):	# check if output folder for frame exists
+
+        if not os.path.exists(self.frames_outdir):
             os.mkdir(self.frames_outdir)
 
-        cmd_str = 'ffmpeg -i "%s" -r 1/1 -s 224x224 -f image2 %s/' % (self.filepath+'.mp4', self.frames_outdir) + '%d.bmp'	# 用ffmpeg extract video中的frame
-        print(cmd_str)
-        os.system(cmd_str)	# execute the command in a subshell
+        extract_frame_cmd = f'ffmpeg -i "{self.filepath}.mp4" -r 1/1 -s 224x224 -f image2 {self.frames_outdir}/%d.bmp'
+        os.system(extract_frame_cmd)
 
     def comments(self):
         """Extract comments from `.ass` file and save it in `tmp/comment.txt`"""
-        with open(self.comments_outfile, 'w', encoding='utf8') as fw:	# open comment output file
-            for line in open(self.filepath+'.ass', 'r', encoding='utf8').readlines():	# open .ass file
-                if line.startswith('Dialogue:'):
-                    comment = line[line.rfind('}')+1:].strip()	# 找到最右邊 } 的index
-                    time = line[line.find(',')+1:line.find('.')]	# time在第一個,至第一個.中間
-                    time = sum(x * int(t) for x, t in zip([3600, 60, 1], time.split(":")))	# 計算總秒數
-                    fw.write("%d\t%d\t%s\n" % (self.video_id, time, comment))
+
+        with open(self.comments_outfile, 'w', encoding='utf8') as out_file:
+            with open(self.filepath+'.ass', 'r', encoding='utf8') as ass_file:
+                comment_id = 0
+                for line in ass_file.readlines():
+                    if line.startswith('Dialogue'):
+                        comment = line[line.rfind('}')+1:].strip()
+                        time = line[line.find(',')+1:line.find('.')]
+                        time = sum(x * int(t) for x, t in zip([3600, 60, 1], time.split(":")))
+                        out_file.write(f'{self.video_id}\t{time}\t{comment}\t{comment_id}\n')
+                        comment_id += 1
